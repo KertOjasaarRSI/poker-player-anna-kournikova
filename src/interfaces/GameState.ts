5cd15014e0e14909e887afc2398e4dde188d1c06
Andre Toodre <toodre@rushstreetinteractive.com>
2022-11-16 13:30:04 +0200

    Update GameState

--- src/interfaces/GameState.ts
+++ src/interfaces/GameState.ts
@@ -2,31 +2,29 @@ export interface GameState {
     tournament_id: string;
     game_id: string;
     round: number;
-    players: Player[];
+    bet_index: number;
     small_blind: number;
-    big_blind: number;
-    orbits: number;
-    dealer: number;
-    community_cards: any[];
     current_buy_in: number;
     pot: number;
-    in_action: number;
     minimum_raise: number;
-    bet_index: number;
+    dealer: number;
+    orbits: number;
+    in_action: number;
+    players: Player[];
+    community_cards: Card[];
+}
+
+export interface Card {
+    rank: string;
+    suit: string;
 }
 
 export interface Player {
-    name: string;
-    stack: number;
-    status: string;
-    bet: number;
-    time_used: number;
-    version: string;
     id: number;
-    hole_cards?: HoleCard[];
-}
-
-export interface HoleCard {
-    rank: string;
-    suit: string;
+    name: string;
+    status: string;
+    version: string;
+    stack: number;
+    bet: number;
+    hole_cards?: Card[];
 }
