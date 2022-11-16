cd40ad2bb7634adb9d65a4fdb40e4e4f7f9ce8b2
Andre Toodre <toodre@rushstreetinteractive.com>
2022-11-16 10:57:04 +0200

    Remove Player logs

--- Player.js
+++ Player.js
@@ -4,13 +4,10 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    console.debug('betRequest - gameState', gameState);
-    console.debug('betRequest - bet', bet);
     bet(1000);
   }
 
   static showdown(gameState) {
-    console.debug('showdown - gameState', gameState);
   }
 }
 
