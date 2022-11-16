f6931b2edcf3145968602ba859a7d98e80820d79
Luis Colubi <lcolubi@rushstreetinteractive.com>
2022-11-16 14:15:34 +0100

    Implement straight

--- src/HandDetector.ts
+++ src/HandDetector.ts
@@ -18,7 +18,47 @@ function detectHandRankings(cards: Card[]) {
   const isFlush = suitBasedCards.some((x) => x.length === 5);
   const isCloseToFlush = suitBasedCards.some((x) => x.length === 4) && cards.length < 7;
 
+  const numericRankKeysSorted = Object.keys(rankBased).map((rank) => {
+    if (rank === 'J') return 11;
+    if (rank === 'Q') return 12;
+    if (rank === 'K') return 12;
+    if (rank === 'A') return 14;
+    return Number(rank);
+  }).sort((a, b) => a - b);
+  
+  const numericRankString = numericRankKeysSorted.join('');
+  
+  const isLowStraight = numericRankKeysSorted[numericRankKeysSorted.length - 1] === 14 
+    && numericRankString.includes('2345');
+  
+  const isTopStraight = numericRankString.includes('1011121314');
+  
+  const isStraight = numericRankKeysSorted.length > 4 && 
+  (isLowStraight 
+   || numericRankString.includes('23456')
+   || numericRankString.includes('34567') 
+   || numericRankString.includes('45678')
+   || numericRankString.includes('56789')
+   || numericRankString.includes('678910')
+   || numericRankString.includes('7891011')
+   || numericRankString.includes('89101112')
+   || numericRankString.includes('910111213')
+   || numericRankString.includes('910111213')
+   || isTopStraight
+  );
+  
+  const isRoyalFlush = isTopStraight && isFlush;
+  const isStraightFlush = isStraight && isFlush;
+
   let value = 10;
+  if (isRoyalFlush) {
+    // Royal flush
+    value = 1
+  }
+  else if (isStraightFlush) {
+    // Straight flush
+    value = 2;
+  }
   if (rankBasedCards.some((x) => x.length === 4)) {
     // Four of a Kind
     value = 3;
@@ -30,11 +70,13 @@ function detectHandRankings(cards: Card[]) {
     // Full House
     value = 4;
   }
-  else if (
-    isFlush
-  ) {
+  else if (isFlush) {
     // Flush
     value = 5
+  }
+  else if (isStraight) {
+    // Straight
+    value = 6;
   }
   else if (rankBasedCards.some((x) => x.length === 3)) {
     // Three of a kind
@@ -50,48 +92,6 @@ function detectHandRankings(cards: Card[]) {
   } else {
     // High card
     value = 10;
-  }
-
-  try {
-    const numericRankKeysSorted = Object.keys(rankBased).map((rank) => {
-      if (rank === 'J') return 11;
-      if (rank === 'Q') return 12;
-      if (rank === 'K') return 12;
-      if (rank === 'A') return 14;
-      return Number(rank);
-    }).sort((a, b) => a - b);
-    
-    const numericRankString = numericRankKeysSorted.join('');
-    
-    const isLowStraight = numericRankKeysSorted[numericRankKeysSorted.length - 1] === 14 
-      && numericRankString.includes('2345');
-    
-    const isTopStraight = numericRankString.includes('1011121314');
-    
-    const isStraight = numericRankKeysSorted.length > 4 && 
-    (isLowStraight 
-     || numericRankString.includes('23456')
-     || numericRankString.includes('34567') 
-     || numericRankString.includes('45678')
-     || numericRankString.includes('56789')
-     || numericRankString.includes('678910')
-     || numericRankString.includes('7891011')
-     || numericRankString.includes('89101112')
-     || numericRankString.includes('910111213')
-     || numericRankString.includes('910111213')
-     || isTopStraight
-    );
-    
-    const isRoyalFlush = isTopStraight && isFlush;
-    const isStraightFlush = isStraight && isFlush;
-    
-    console.log('[LUIS LOGS] RANK SORTED NUMBERS ARRAY', numericRankKeysSorted);
-    console.log('[LUIS LOGS] RANK SORTED STRING', numericRankString);
-    console.log('[LUIS LOGS] isStraight', isStraight);
-    console.log('[LUIS LOGS] isRoyalFlush', isRoyalFlush);
-    console.log('[LUIS LOGS] isStraightFlush', isStraightFlush);
-  } catch (e) {
-    console.log('[LUIS LOGS] ERROR', e)
   }
 
   return value;
