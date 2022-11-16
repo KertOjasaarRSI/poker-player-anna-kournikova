7a2bfb3d1aa10cebe71ab271a17d34de514d54e7
Kert Ojasaar <ojasaar@rushstreetinteractive.com>
2022-11-16 11:05:12 +0200

    More organised bet #2

--- Player.js
+++ Player.js
@@ -1,60 +1,52 @@
 class Player {
-  static action = {
-    FOLD: 0,
-    CALL: 1,
-    RAISE: 2,
-    ALL_IN: 3,
-  }
-
-  static combinations = {
-    'AA': 3,
-    'KK': 3,
-    'QQ': 3,
-    'JJ': 3,
-    'TT': 3,
-    '99': 3,
-    '88': 3,
-    '77': 3,
-    '66': 3,
-    '55': 3,
-    '44': 3,
-    '33': 3,
-    '22': 3,
-
-    'KA': 3,
-    'AK': 3,
-    'AQ': 3,
-    'QA': 3,
-    'AJ': 3,
-    'JA': 3,
-    'A10': 3,
-    '10A': 3,
-    'A9': 3,
-    '9A': 3,
-    'A8': 3,
-    '8A': 3,
-    'A7': 3,
-    '7A': 3,
-    'A6': 3,
-    '6A': 3,
-    'A5': 3,
-    '5A': 3,
-    'A4': 3,
-    '4A': 3,
-    'A3': 3,
-    '3A': 3,
-    'A2': 3,
-    '2A': 3,
-  }
-
   static get VERSION() {
     return '0.2';
   }
 
   static betRequest(gameState, bet) {
+    const combinations = {
+      'AA': 3,
+      'KK': 3,
+      'QQ': 3,
+      'JJ': 3,
+      'TT': 3,
+      '99': 3,
+      '88': 3,
+      '77': 3,
+      '66': 3,
+      '55': 3,
+      '44': 3,
+      '33': 3,
+      '22': 3,
+
+      'KA': 3,
+      'AK': 3,
+      'AQ': 3,
+      'QA': 3,
+      'AJ': 3,
+      'JA': 3,
+      'A10': 3,
+      '10A': 3,
+      'A9': 3,
+      '9A': 3,
+      'A8': 3,
+      '8A': 3,
+      'A7': 3,
+      '7A': 3,
+      'A6': 3,
+      '6A': 3,
+      'A5': 3,
+      '5A': 3,
+      'A4': 3,
+      '4A': 3,
+      'A3': 3,
+      '3A': 3,
+      'A2': 3,
+      '2A': 3,
+    };
     const cards = gameState.players.find(({ hole_cards }) => hole_cards);
     const cardsString = `${cards[0].rank}${cards[1].rank}`.toUpperCase();
-    if (this.combinations[cardsString]) {
+    if (combinations[cardsString]) {
       console.log(`cards: ${cardsString}, ALL IN`);
       bet(1000);
     } else {
